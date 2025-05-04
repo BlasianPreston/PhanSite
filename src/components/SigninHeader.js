00300de922b28d3e9bb54425fe58f288ed0a6d77
Isiah Williams <84547105+BlasianPreston@users.noreply.github.com>
2025-05-04 14:00:01 -0400

    worked on more code

---
+++ src/components/SigninHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import SLogo from '../images/phantoms.jpg';
+import '../styles/signinheader.css'
+
+export default function Signinheader() {
+    return (
+            <nav>
+                <Link href='/'><Image src={SLogo} alt='Phantom Thieves Logo' className='logo'></Image></Link>
+                <div className='account-pages'>
+                    <Link href='/SignIn'><h3>Log In</h3></Link>
+                    <Link href='signup'><h3>Sign In</h3></Link>
+                </div>    
+            </nav>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
-import SLogo from '../images/phantoms.jpg';
 import BLogo from '../images/phantoms_big.jpg'
+import Footer from '../components/Footer.js'
+import Signinheader from '../components/SigninHeader';
 import '../styles/home.css'
 
 export default function Home() {
     return(
-        <div>
-            <nav>
-                <Image src={SLogo} alt='Phantom Thieves Logo' className='logo'></Image>
-                <div className='account-pages'>
-                    <h3>Log In</h3>
-                    <h3>Sign In</h3>
-                </div>    
-            </nav>
+        <div className='page'>
+            <Signinheader />
             <div className='text'>
                 <h2>One Stop Site for the Phantom Thieves</h2>
                 <Image src={BLogo} alt='Phantom Thieves Logo Big'></Image>
                 <h4>Need a change of heart?</h4>
                 <h5>They've got you covered!</h5>
-            </div>        
+            </div>
+            <Footer />        
         </div>
     )
 }
