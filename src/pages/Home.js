020fdf20ea650914f618e5600d9d6961204e16ea
Isiah Williams <84547105+BlasianPreston@users.noreply.github.com>
2025-05-03 21:45:42 -0400

    home page layout complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,10 +14,11 @@ export default function Home() {
                     <h3>Sign In</h3>
                 </div>    
             </nav>
-            <div>
+            <div className='text'>
                 <h2>One Stop Site for the Phantom Thieves</h2>
+                <Image src={BLogo} alt='Phantom Thieves Logo Big'></Image>
                 <h4>Need a change of heart?</h4>
-                <h5>They've got you covered</h5>
+                <h5>They've got you covered!</h5>
             </div>        
         </div>
     )
