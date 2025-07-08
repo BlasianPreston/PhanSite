dc2d4274607a4a4a570e384c8aba83ea21cb4e0a
Preston Williams <ipw5@cornell.edu>
2025-07-07 22:01:03 -0400

    modularized cards for post page

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import Logo from '../../images/phantoms.jpg'
+import PostCard from '../../components/PostCard.js'
 import '../../styles/posts.css';
 
 export default function Posts() {
     return (
         <div className='page'>
             <h1 className='title'>Posts</h1>
-            <div className='card'>
-                    <h1>Phantom Thieves</h1>
-                    <Image src={Logo} alt ='Phantom Thieves Logo'/>
-                    <h3>This is a post about the Phantom Thieves</h3>
-                    <h4>Made by: User</h4>
-            </div>
+            <PostCard title="Phantom Thieves" image={Logo} description="Sample Description" user="User"/>
         </div>
     )
 }
---
+++ src/components/PostCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import '../styles/post_card.css';
+
+export default function PostCard(props) {
+    return (
+        <div className='card'>
+            <h1>{props.title}</h1>
+            <Image src={props.image} alt ='Phantom Thieves Logo'/>
+            <h3>{props.description}</h3>
+            <h4>Made by: {props.user}</h4>
+        </div>
+    )
+}
